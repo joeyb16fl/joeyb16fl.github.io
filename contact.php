a228f97d06384aa98e8fea1def1eebe9cb0fcff9
Joseph Bartholomew <joeyb16fl@gmail.com>
2018-08-23 13:29:08 -0400

    Current Contact.php file

---
+++ contact.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+		
+        <meta name="keywords" content="Sports" />
+        <meta name="description" content="Athletic League Management Tool" />
+        <meta name="author" content="myoplan.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<title>MyoPlan Sports</title>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon.png" />
+        <!-- Font -->
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Arimo:300,400,700,400italic,700italic' />
+        <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css' />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+        <!-- Font Awesome Icons -->
+        <link href='css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/hover-dropdown-menu.css" rel="stylesheet" />
+        <!-- Icomoon Icons -->
+        <link href="css/icons.css" rel="stylesheet" />
+        <!-- Revolution Slider -->
+        <link href="css/revolution-slider.css" rel="stylesheet" />
+        <link href="rs-plugin/css/settings.css" rel="stylesheet" />
+        <!-- Animations -->
+        <link href="css/animate.min.css" rel="stylesheet" />
+        <!-- Owl Carousel Slider -->
+        <link href="css/owl/owl.carousel.css" rel="stylesheet" />
+        <link href="css/owl/owl.theme.css" rel="stylesheet" />
+        <link href="css/owl/owl.transitions.css" rel="stylesheet" />
+        <!-- PrettyPhoto Popup -->
+        <link href="css/prettyPhoto.css" rel="stylesheet" />
+        <!-- Custom Style -->
+        <link href="css/style.css" rel="stylesheet" />
+		<link href="css/responsive.css" rel="stylesheet">
+        <!-- Color Scheme -->
+        <link href="css/color.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/pdfmake-0.1.18/dt-1.10.13/b-1.2.4/b-colvis-1.2.4/b-flash-1.2.4/b-html5-1.2.4/b-print-1.2.4/cr-1.3.2/fh-3.1.2/kt-2.2.0/r-2.1.0/rr-1.2.0/sc-1.4.2/se-1.2.0/datatables.min.css"/>
+ 
+    </head>
+    <body>
+       <!-- Start of HubSpot Embed Code -->
+         <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/2718514.js"></script>
+       <!-- End of HubSpot Embed Code -->
+    <div id="page">
+		<!-- Page Loader -->
+		<div id="pageloader">
+			<div class="loader-item fa fa-spin text-color"></div>
+		</div>
+        <!-- Top Bar -->
+        <div id="top-bar" class="top-bar-section top-bar-bg-color">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <!-- Top Contact -->
+                        <div class="top-contact link-hover-black">
+                        <a href='contact.php'>
+                        <i class="fa fa-phone"></i> (844) 2000-MYO</a> 
+                        <a href='contact.php'>
+                        <i class="fa fa-envelope"></i>support@myoplansports.com</a></div>
+                        <!-- Top Social Icon -->
+                        <div class="top-social-icon icons-hover-black">
+                        <a href="https://www.facebook.com/myoplansports">
+                            <i class="fa fa-facebook"></i>
+                        </a> 
+                        <a href="https://www.twitter.com/myoplansports">
+                            <i class="fa fa-twitter"></i>                     
+                        </a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Top Bar -->
+        <!-- Sticky Navbar -->
+         <header id="sticker" class="sticky-navigation">
+			<!-- Sticky Menu -->
+            <div class="sticky-menu relative">
+				<!-- navbar -->
+				<div class="navbar navbar-default navbar-bg-light" role="navigation">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="navbar-header">
+								<!-- Button For Responsive toggle -->
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span> 
+								<span class="icon-bar"></span> 
+								<span class="icon-bar"></span> 
+								<span class="icon-bar"></span></button> 
+								<!-- Logo -->
+								 
+								<a class="navbar-brand" href='http://www.myoplan.com/index.html'>
+									<img class="site_logo" alt="Site Logo" width="190" height="86" src="img/logo.png" style="margin-top:4px"/>
+								</a></div>
+								<!-- Navbar Collapse -->
+								<div class="navbar-collapse collapse">
+									<!-- nav -->
+									<ul class="nav navbar-nav">
+										<!-- Home  Mega Menu -->
+										<li class="mega-menu">
+											<a href="index.php">Home</a>
+											
+										</li>
+										<!-- Mega Menu Ends -->
+										<!-- Pages Mega Menu -->
+										<!--li class="mega-menu">
+											<a href="about.php">About Us</a>
+											</li-->
+										<!-- Pages Menu Ends -->
+										<!-- Portfolio Menu -->
+										<li>
+										<a href="contact.php">Contact</a> 
+										<li>
+										
+										<!-- Blog Menu -->
+										
+										<!-- Blog Menu -->
+										<!-- Contact Block -->
+										<li>
+											<a href="login.php">Login</a>
+										</li>
+										
+										<!-- <li class="hidden-767"> -->
+											<!-- <a href="#" class="header-search"> -->
+												<!-- <span> -->
+													<!-- <i class="fa fa-search"></i> -->
+												<!-- </span> -->
+											<!-- </a> -->
+										<!-- </li> -->
+									</ul>
+									<!-- Right nav -->
+									<!-- Header Contact Content -->
+									<div class="bg-white hide-show-content no-display header-contact-content">
+										<p class="vertically-absolute-middle">Call Us 
+										<strong>+0 (123) 456 78 90</strong></p>
+										<button class="close">
+											<i class="fa fa-times"></i>
+										</button>
+									</div>
+									<!-- Header Contact Content -->
+									<!-- Header Search Content -->
+									<div class="bg-white hide-show-content no-display header-search-content">
+										<form role="search" class="navbar-form vertically-absolute-middle">
+											<div class="form-group">
+												<input type="text" placeholder="Enter your text &amp; Search Here" class="form-control" id="s" name="s" value="" />
+											</div>
+										</form>
+										<button class="close">
+											<i class="fa fa-times"></i>
+										</button>
+									</div>
+									<!-- Header Search Content -->
+									<!-- Header Share Content -->
+									<div class="bg-white hide-show-content no-display header-share-content">
+										<div class="vertically-absolute-middle social-icon gray-bg icons-circle i-3x">
+										<a href="#">
+											<i class="fa fa-facebook"></i>
+										</a> 
+										<a href="#">
+											<i class="fa fa-twitter"></i>
+										</a> 
+										<a href="#">
+											<i class="fa fa-pinterest"></i>
+										</a> 
+										<a href="#">
+											<i class="fa fa-google"></i>
+										</a> 
+										<a href="#">
+											<i class="fa fa-linkedin"></i>
+										</a></div>
+										<button class="close">
+											<i class="fa fa-times"></i>
+										</button>
+									</div>
+									<!-- Header Share Content -->
+								</div>
+								<!-- /.navbar-collapse -->
+							</div>
+							<!-- /.col-md-12 -->
+						</div>
+						<!-- /.row -->
+					</div>
+					<!-- /.container -->
+				</div>
+				<!-- navbar -->
+			</div>
+			 <!-- Sticky Menu -->
+		</header>
+
+ <div class="page-header">
+                <div class="container">
+                    <h1 class="title">Contact Us</h1>
+                </div>
+                <div class="breadcrumb-box">
+                    <div class="container">
+                        <ul class="breadcrumb">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="#">Pages</a></li>
+                            <li class="active">Contact Us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- page-header -->
+            <section id="contact-us" class="page-section">
+                <div class="container">
+					<div class="row">
+                        <div class="col-sm-6 col-md-8">
+							<div class="row">
+								<div class="col-sm-6 col-md-4">
+									<h5 class="title"><i class="icon-phone10 text-color"></i> Phone</h5>
+									<div>(844) 2000-MYO (200-0696)</div>
+									
+								</div>
+								<div class="col-sm-6 col-md-6">
+									<h5 class="title"><i class="icon-envelope7 text-color"></i> Email</h5>
+									<div>Product Info: <b>info@myoplansports.com</b><br> 
+										Schedule a demo: <b>demo@myoplansports.com</b> <br>
+										Technical Support: <b>support@myoplansports.com</b> </div>
+									
+								</div>
+							</div>
+							<hr>
+							MyoPlan Sports is aimed at keeping sports fun by taking care of all the hard stuff for you. </p>
+							<p>MyoPlan Sports is the ultimate athletic registration software for gyms, recreation centers, youth leagues, personal trainers, church leagues, and school athletic programs.  We exist to allow you to build rosters and register your athletes in a cost and time efficient manner. </p> <hr>
+							<p>MyoPlan's core features include a Registration Tool and a Roster Builder. </p>
+							<p>Our current features are available for our August 2018 launch.  We also have an eye on the future and have many state-of-the-art features scheduled for future release.  </p>
+							
+						</div>
+						<div class="col-md-6 col-md-4">							
+							<h3 class="title">Schedule <span style="text-transform:none !important">a</span> Demo</h3>
+							<p class="form-message" style="display: none;"></p>
+							<div class="contact-form">
+								<!-- Form Begins -->
+								<form role="form" name="contact-form" id="contact-form" method="post" action="contact_form/contact_process.php">
+										
+										<div class="input-text form-group">
+											<input type="text" name="first_name" class="input-name form-control" placeholder="First Name" />
+										</div>
+										<div class="input-text form-group">
+											<input type="text" name="contact_lname" class="input-name form-control" placeholder="Last Name" />
+										</div>
+										
+										<div class="input-text form-group">
+											<input type="text" name="contact_oname" class="input-name form-control" placeholder="Organization" />
+										</div>								
+										
+										<div class="input-email form-group">
+											<input type="text" name="contact_phone" class="input-phone form-control" placeholder="Phone"/>
+										</div>
+										<div class="input-email form-group">
+											<input type="email" name="contact_email" class="input-email form-control" placeholder="Email"/>
+										</div>
+										
+		
+										<div class="textarea-message form-group">
+											<textarea name="contact_message" class="textarea-message form-control" placeholder="Message" rows="2" ></textarea>
+										</div>
+										<!-- Button -->
+										<button class="btn btn-default" type="submit">Send Now <i class="icon-paper-plane"></i></button>
+									</div>
+								</form>
+								<!-- Form Ends -->
+							</div>
+					</div>
+                </div>
+            </section><!-- page-section -->
+			
+			
+			
+			
+			     <!-- request -->
+        <footer id="footer">
+            <div class="footer-widget">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 col-md-3 widget bottom-xs-pad-20">
+                            <div class="widget-title">
+                                <!-- Title -->
+                                <h3 class="title"></h3>
+                            </div>
+                            <!-- Text -->
+                            <!-- <p>We strive to help your league run smoothly, so if you wish to see how we can help your league, rec center or school please reach out by email or phone to:</p> -->
+                            <!-- Address -->
+                            <p>
+                            <strong>Office:</strong> 844-2000-MYO (200-0696)
+                            </p>
+                            <!-- Phone -->
+                            <p>
+                            <strong>Email:</strong> info@myoplansports.com
+                            </p>
+							
+			<!-- <p>
+			    	<strong>BETA Signup:</strong>  To inquire about joining our current Beta testing group, please email us at info@myoplansports.com
+			    </p> -->
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 widget bottom-xs-pad-20">
+                            
+                            
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-3 widget">
+                            
+                        </div>
+                        <!-- <div class="col-xs-12 col-sm-6 col-md-3 widget newsletter bottom-xs-pad-20"> -->
+                            <!-- <div class="widget-title"> -->
+                                <!-- Title -->
+                                <!-- <h3 class="title">Newsletter Signup</h3> -->
+                            <!-- </div> -->
+                            <!-- <div> -->
+                                <!-- Text -->
+                                <!-- <p>Subscribe to Our Newsletter to get Important News, Amazing Offers &amp; Inside Scoops:</p> -->
+                                <!-- <p class="form-message1" style="display: none;"></p> -->
+                                <!-- <div class="clearfix"></div> -->
+                                <!-- Form -->
+                                <!-- <form id="subscribe_form" action="http://www.myoplan.com/contact.php" method="post" name="subscribe_form" role="form"> -->
+                                    <!-- <div class="input-text form-group has-feedback"> -->
+                                    <!-- <input class="form-control" type="email" value="" name="subscribe_email" />  -->
+                                    <!-- <button class="submit bg-color" type="submit"> -->
+                                        <!-- <span class="glyphicon glyphicon-arrow-right"></span> -->
+                                   <!--  </button></div> -->
+                                <!-- </form> -->
+                            <!-- </div> -->
+                            <!-- Count -->
+                            
+                            <!-- Social Links -->
+                            <div class="social-icon gray-bg icons-circle i-3x">
+                            <a href="https://www.facebook.com/myoplansports">
+                                <i class="fa fa-facebook"></i>
+                            </a> 
+                            <a href="https://www.twitter.com/myoplansports">
+                                <i class="fa fa-twitter"></i>
+                            </a> 
+                            
+                            <a href="https://plus.google.com/u/0/104781402791522680094">
+                                <i class="fa fa-google"></i>
+                            </a> 
+                            </div>
+                        </div>
+                        <!-- .newsletter -->
+                    </div>
+                </div>
+            </div>
+            <!-- footer-top -->
+            <div class="copyright">
+                <div class="container">
+                    <div class="row">
+                        <!-- Copyrights -->
+                        <div class="col-xs-10 col-sm-6 col-md-6"> &copy; 2018  Made by <a href="http://myoplansports.com/contact">MyoPlan Sports</a>.
+                        <br />
+                        <!-- Terms Link -->
+                         
+                        <!-- <a href="#">Terms of Use</a> /  -->
+                        <!-- <a href="#">Privacy Policy</a></div> -->
+                        <!-- <div class="col-xs-2 col-sm-6 col-md-6 text-right page-scroll gray-bg icons-circle i-3x"> -->
+                            <!-- Goto Top -->
+                            <a href="#page">
+                                <i class="glyphicon glyphicon-arrow-up"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- footer-bottom -->
+        </footer>
+        <!-- footer -->
+    </div>
+    <!-- page -->
+    <!-- Scripts -->
+    
+    <script type="text/javascript" src="js/hover-dropdown-menu.js"></script> 
+    <!-- Menu jQuery Bootstrap Addon --> 
+    <script type="text/javascript" src="js/jquery.hover-dropdown-menu-addon.js"></script> 
+    <!-- Scroll Top Menu -->
+     
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script> 
+    <!-- Sticky Menu --> 
+    <script type="text/javascript" src="js/jquery.sticky.js"></script> 
+    <!-- Bootstrap Validation -->
+     
+    <script type="text/javascript" src="js/bootstrapValidator.min.js"></script> 
+    <!-- Revolution Slider -->
+     
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script> 
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script> 
+    <script type="text/javascript" src="js/revolution-custom.js"></script> 
+    <!-- Portfolio Filter -->
+     
+    <script type="text/javascript" src="js/jquery.mixitup.min.js"></script> 
+    <!-- Animations -->
+     
+    <script type="text/javascript" src="js/jquery.appear.js"></script> 
+    <script type="text/javascript" src="js/effect.js"></script> 
+    <!-- Owl Carousel Slider -->
+     
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script> 
+    <!-- Pretty Photo Popup -->
+     
+    <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script> 
+    <!-- Parallax BG -->
+     
+    <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script> 
+    <!-- Fun Factor / Counter -->
+     
+    <script type="text/javascript" src="js/jquery.countTo.js"></script> 
+    <!-- Twitter Feed -->
+     
+    <script type="text/javascript" src="js/tweet/carousel.js"></script> 
+    <script type="text/javascript" src="js/tweet/scripts.js"></script> 
+    <script type="text/javascript" src="js/tweet/tweetie.min.js"></script> 
+    <!-- Background Video -->
+     
+    <script type="text/javascript" src="js/jquery.mb.YTPlayer.js"></script> 
+    <!-- Custom Js Code -->
+     
+    <script type="text/javascript" src="js/custom.js"></script> 
+    <!-- Scripts --></body>
+</html>
